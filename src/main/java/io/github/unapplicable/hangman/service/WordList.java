8ace54c82ed9c486ded84e2056c12800ee5cec2f
lauri <lauri.nomme@gmail.com>
2020-10-09 15:08:58 +0300

    fix warning

--- src/main/java/io/github/unapplicable/hangman/service/WordList.java
+++ src/main/java/io/github/unapplicable/hangman/service/WordList.java
@@ -3,6 +3,7 @@ package io.github.unapplicable.hangman.service;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.PrimitiveIterator;
 import java.util.Random;
@@ -13,7 +14,7 @@ public class WordList {
     private final PrimitiveIterator.OfInt randomIterator;
 
     public WordList(InputStream stream) {
-        this(new BufferedReader(new InputStreamReader(stream))
+        this(new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))
             .lines()
             .map(String::toLowerCase)
             .collect(Collectors.toList()));
